9f04b77c3a39985cebd5c7c0d80c79506665e2c8
Dong Chi <21130295@st.hcmuaf.edu.vn>
2025-06-20 16:11:07 +0700

    update

--- src/test/java/com/example/beprojectweb/BeProjectWebApplicationTests.java
+++ src/test/java/com/example/beprojectweb/BeProjectWebApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.beprojectweb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class BeProjectWebApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
