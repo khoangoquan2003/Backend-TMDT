7457abd0c2484ee718384ead5c9b7bc309fea0cb
Khoang Quan <your-email@example.com>
2025-06-22 19:24:07 +0700

    up

--- src/main/java/com/example/beprojectweb/mapper/CategoryMapper.java
+++ src/main/java/com/example/beprojectweb/mapper/CategoryMapper.java
@@ -1,19 +1,28 @@
 package com.example.beprojectweb.mapper;
 
-import com.example.beprojectweb.dto.request.UserUpdateRequest;
 import com.example.beprojectweb.dto.request.category.CategoryRequest;
 import com.example.beprojectweb.dto.request.category.CategoryUpdateRequest;
 import com.example.beprojectweb.dto.response.category.CategoryResponse;
 import com.example.beprojectweb.entity.Category;
-import com.example.beprojectweb.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
+import java.util.UUID;
+
+
 @Mapper(componentModel = "spring", uses = {ProductMapper.class})
 public interface CategoryMapper {
+
     Category category(CategoryRequest request);
-    @Mapping(source = "productList", target = "productList")  // map List<Product> -> List<ProductResponse>
+
+    @Mapping(source = "productList", target = "productList")
     CategoryResponse toCategoryResponse(Category category);
+
+    // Update category method with mapping for fields
+    @Mapping(source = "request.name", target = "name")
+    @Mapping(source = "request.description", target = "description")
+    @Mapping(source = "request.urlImage", target = "urlImage")
     void updateCategory(@MappingTarget Category category, CategoryUpdateRequest request);
 }
+
